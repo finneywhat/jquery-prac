44a0143d736a685a446b3788421a196569d5e765
Chris Finney and Dallas Slaughter <chris@>
2017-02-14 09:15:25 -0800

    create jquery clickable sections

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
     $("#kek").fadeToggle();
     $("#funny-haha").slideToggle();
   });
+  $("#ponies-click").click(function() {
+    $("#p-in-ponies").fadeIn();
+  });
+  $("#clickable-h3").click(function() {
+    $("#lorem-ip-p, #lorem-ip-pic").slideToggle();
+    // $("lorem-ip-pic").slideDown()
+  });
 });

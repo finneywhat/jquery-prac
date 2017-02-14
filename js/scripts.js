59ab22a48ba4d14c5b9ce2eb44bc1ea31c3d4f68
Chris Finney and Dallas Slaughter <chris@>
2017-02-14 10:11:26 -0800

    Add jQuery function to toggle borders on and off by adding a class

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#kek").fadeToggle();
-    $("#funny-haha").slideToggle();
-  });
-  $("#ponies-click").click(function() {
-    $("#p-in-ponies").fadeIn();
-  });
-  $("#clickable-h3").click(function() {
-    $("#lorem-ip-p, #lorem-ip-pic").slideToggle();
-    // $("lorem-ip-pic").slideDown()
-  });
-  $("#die-lorem > h3").click(function(){
+  // $(".clickable").click(function() {
+  //   $("#kek").fadeToggle();
+  //   $("#funny-haha").slideToggle();
+  // });
+  // $("#ponies-click").click(function() {
+  //   $("#p-in-ponies").fadeIn();
+  // });
+  // $("#clickable-h3").click(function() {
+  //
+  //   $("#lorem-ip-p, #lorem-ip-pic").slideToggle();
+  //
+  //   // $("lorem-ip-pic").slideDown()
+  // });
+  $("#die-lorem h3").click(function() {
+    $("p").toggleClass("add-border");
     $("#die-lorem > p").slideUp("slow");
   });
 });
